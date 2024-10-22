f6e04a09de62bed8afa3f4715b20d2d30ee4af87
Flubberschnub <kw6561@gmail.com>
2024-10-22 14:35:05 -0500

    main first page

---
+++ PrivaSee/app/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BeginningScreen from './firstscreen';
+
+export default function HomeScreen() {
+  return (
+    <BeginningScreen />
+  );
+}
---
+++ PrivaSee/app/themes.tsx
@@ -0,0 +1,150 @@
+import { configureFonts } from "react-native-paper";
+
+const fontConfig = {
+  web: {
+    regular: {
+      fontFamily: "Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+      fontWeight: "400",
+    },
+    medium: {
+      fontFamily: "Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+      fontWeight: "500",
+    },
+    light: {
+      fontFamily: "Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+      fontWeight: "300",
+    },
+    thin: {
+      fontFamily: "Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+      fontWeight: "100",
+    },
+  },
+  ios: {
+    regular: {
+      fontFamily: "System",
+      fontWeight: "400",
+    },
+    medium: {
+      fontFamily: "System",
+      fontWeight: "500",
+    },
+    light: {
+      fontFamily: "System",
+      fontWeight: "300",
+    },
+    thin: {
+      fontFamily: "System",
+      fontWeight: "100",
+    },
+  },
+  android: {
+    regular: {
+      fontFamily: "Roboto",
+      fontWeight: "400",
+    },
+    medium: {
+      fontFamily: "Roboto",
+      fontWeight: "500",
+    },
+    light: {
+      fontFamily: "Roboto",
+      fontWeight: "300",
+    },
+    thin: {
+      fontFamily: "Roboto",
+      fontWeight: "100",
+    },
+  },
+} as const;
+
+export const privaseeTheme = {
+  colors: {
+    primary: "rgb(71, 85, 182)",
+    onPrimary: "rgb(255, 255, 255)",
+    primaryContainer: "rgb(223, 224, 255)",
+    onPrimaryContainer: "rgb(0, 13, 95)",
+    secondary: "rgb(135, 82, 0)",
+    onSecondary: "rgb(255, 255, 255)",
+    secondaryContainer: "rgb(255, 221, 186)",
+    onSecondaryContainer: "rgb(43, 23, 0)",
+    tertiary: "rgb(0, 103, 131)",
+    onTertiary: "rgb(255, 255, 255)",
+    tertiaryContainer: "rgb(188, 233, 255)",
+    onTertiaryContainer: "rgb(0, 31, 42)",
+    error: "rgb(186, 26, 26)",
+    onError: "rgb(255, 255, 255)",
+    errorContainer: "rgb(255, 218, 214)",
+    onErrorContainer: "rgb(65, 0, 2)",
+    background: "rgb(255, 251, 255)",
+    onBackground: "rgb(27, 27, 31)",
+    surface: "rgb(255, 251, 255)",
+    onSurface: "rgb(27, 27, 31)",
+    surfaceVariant: "rgb(227, 225, 236)",
+    onSurfaceVariant: "rgb(70, 70, 79)",
+    outline: "rgb(118, 118, 128)",
+    outlineVariant: "rgb(199, 197, 208)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(48, 48, 52)",
+    inverseOnSurface: "rgb(243, 240, 244)",
+    inversePrimary: "rgb(187, 195, 255)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(246, 243, 251)",
+      level2: "rgb(240, 238, 249)",
+      level3: "rgb(235, 233, 247)",
+      level4: "rgb(233, 231, 246)",
+      level5: "rgb(229, 228, 245)",
+    },
+    surfaceDisabled: "rgba(27, 27, 31, 0.12)",
+    onSurfaceDisabled: "rgba(27, 27, 31, 0.38)",
+    backdrop: "rgba(47, 48, 56, 0.4)",
+    fonts: configureFonts({ config: fontConfig, isV3: false }),
+  },
+};
+
+export const privaseeThemeDark = {
+  colors: {
+    primary: "rgb(187, 195, 255)",
+    onPrimary: "rgb(17, 34, 134)",
+    primaryContainer: "rgb(45, 60, 156)",
+    onPrimaryContainer: "rgb(223, 224, 255)",
+    secondary: "rgb(255, 184, 101)",
+    onSecondary: "rgb(72, 42, 0)",
+    secondaryContainer: "rgb(102, 61, 0)",
+    onSecondaryContainer: "rgb(255, 221, 186)",
+    tertiary: "rgb(99, 211, 255)",
+    onTertiary: "rgb(0, 53, 69)",
+    tertiaryContainer: "rgb(0, 77, 99)",
+    onTertiaryContainer: "rgb(188, 233, 255)",
+    error: "rgb(255, 180, 171)",
+    onError: "rgb(105, 0, 5)",
+    errorContainer: "rgb(147, 0, 10)",
+    onErrorContainer: "rgb(255, 180, 171)",
+    background: "rgb(27, 27, 31)",
+    onBackground: "rgb(228, 225, 230)",
+    surface: "rgb(27, 27, 31)",
+    onSurface: "rgb(228, 225, 230)",
+    surfaceVariant: "rgb(70, 70, 79)",
+    onSurfaceVariant: "rgb(199, 197, 208)",
+    outline: "rgb(144, 144, 154)",
+    outlineVariant: "rgb(70, 70, 79)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(228, 225, 230)",
+    inverseOnSurface: "rgb(48, 48, 52)",
+    inversePrimary: "rgb(71, 85, 182)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(35, 35, 42)",
+      level2: "rgb(40, 40, 49)",
+      level3: "rgb(45, 46, 56)",
+      level4: "rgb(46, 47, 58)",
+      level5: "rgb(49, 51, 62)",
+    },
+    surfaceDisabled: "rgba(228, 225, 230, 0.12)",
+    onSurfaceDisabled: "rgba(228, 225, 230, 0.38)",
+    backdrop: "rgba(47, 48, 56, 0.4)",
+    fonts: configureFonts({ config: fontConfig, isV3: false }),
+  },
+};
