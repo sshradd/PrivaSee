de865f3c9612534bd8ddfae89abbab504d91b1e8
Flubberschnub <kw6561@gmail.com>
2024-11-26 14:05:34 -0600

    fixed main layout

--- PrivaSee/app/(tabs)/_layout.tsx
+++ PrivaSee/app/(tabs)/_layout.tsx
@@ -1,29 +1,66 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Slot } from "expo-router";
+import { Header } from "@react-navigation/stack";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
+    // <Tabs
+    //   screenOptions={{
+    //     tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    //     headerShown: false,
+    //   }}>
+    //   <Tabs.Screen
+    //     name="index"
+    //     options={{
+    //       title: 'Home',
+    //       tabBarIcon: ({ color, focused }) => (
+    //         <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+    //       ),
+    //     }}
+    //   />
+
+    // </Tabs>
+
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="appDashboard"
         options={{
-          title: 'Home',
+          title: "Dashboard",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "home" : "home-outline"}
+              color={color}
+            />
           ),
         }}
       />
-      
+      <Tabs.Screen
+        name="selectApps"
+        options={{
+          title: "Select Apps",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "apps" : "apps-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen name="oldindex" options={{ href: null }} />
+      <Tabs.Screen name="settings" options={{ href: null }} />
+      <Tabs.Screen name="dashboard" options={{ href: null }} />
+      <Tabs.Screen name="advancedset" options={{ href: null }} />
     </Tabs>
   );
 }
